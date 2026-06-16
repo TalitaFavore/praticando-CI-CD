b9eb3f35e832cd3b8f88dbc6060066524406e408
TalitaFavore <132314574+TalitaFavore@users.noreply.github.com>
2026-06-16 20:23:13 -0300

    feat: create desafio.js

---
+++ desafio.js
@@ -0,0 +1,21 @@
+// ==========================================
+// DESAFIO: O MISTÉRIO DO NÚMERO 2
+// ==========================================
+// INSTRUÇÕES:
+// 1. Altere o código dentro da função abaixo.
+// 2. Você pode usar soma (+), subtração (-), multiplicação (*), divisão (/) ou misturar tudo!
+// 3. O resultado final retornado pela função DEVE ser exatamente 2.
+// 4. ATENÇÃO: Não vale apenas escrever "return 2;". O robô vai te pegar! Seja criativo(a).
+
+function minhaFuncao() {
+let num1 = 0;
+let num2 = 0;
+let resultado = 0;
+    // ⬇️ ESCREVA SUA FÓRMULA MATEMÁTICA NA LINHA ABAIXO ⬇️
+
+    return resultado; 
+}
+
+// ⚠️ NÃO MEXA NA LINHA ABAIXO! 
+// Ela é a "ponte" que permite ao robô do professor testar o seu código.
+module.exports = minhaFuncao;
